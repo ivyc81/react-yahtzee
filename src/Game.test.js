b81948310e2f7dc22db9b7d36730deeb5d5247f8
Cynthia <ivyc8192@gmail.com>
2019-03-22 14:56:05 -0700

    prevent rolling more than 3 times, disallow reusing score line

--- src/Game.test.js
+++ src/Game.test.js
@@ -17,9 +17,35 @@ it('tests if a die is clicked on, it toggles lock/unlock ', () => {
   const wrapper = mount(<Game />);
 
   const die1 = wrapper.find("Die").first();
-  die1.simulate("click"); 
+  die1.simulate("click");
 
   expect(wrapper.state().locked[0]).toEqual(true);
 });
 
+it('prevents from rolling more than three times', () => {
+  const wrapper = mount(<Game />);
+
+  const rerollBtn = wrapper.find(".Game-reroll").first();
+  rerollBtn.simulate("click");
+  rerollBtn.simulate("click");
+  rerollBtn.simulate("click");
+
+  expect(wrapper.state().locked[0]).toEqual(true);
+
+  const die1 = wrapper.find("Die").first();
+  die1.simulate("click");
+
+  expect(wrapper.state().locked[0]).toEqual(true);
+});
+
+it('disallows re-using score line', () => {
+  const wrapper = mount(<Game />);
+  wrapper.setState({scores:{ones: 9}});
+  // expect(wrapper.state().scores.ones).toEqual(9);
+
+  const scoreLine = wrapper.find(".RuleRow-name").first();
+  scoreLine.simulate("click");
+
+  expect(wrapper.state().scores.ones).toEqual(9);
+});
 
